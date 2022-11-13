69c943dfeab28788fa331942dea7be1b3edbda87
RubyCircle <pavel.jakushik@yandex.by>
2022-11-13 20:28:06 +0300

    add lab9

---
+++ lab9/TPOWebTestsCSharp/WebDriverUnitTest/CeleniumTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using System;
+using System.Threading;
+
+namespace WebDriverUnitTest
+{
+    [TestClass]
+    public class CeleniumTests
+    {
+        [TestMethod]
+        public void QuestionSearchTest()
+        {
+            WebDriver driver = new ChromeDriver();
+            driver.Url = @"https://ru.stackoverflow.com/";
+
+            var findInput = driver.FindElement(By.ClassName("s-input__search"));
+            findInput.SendKeys("C++ emplace_back");
+            findInput.SendKeys(Keys.Return);
+
+            var firstQuestion = driver.FindElement(By.ClassName("s-link"));
+            firstQuestion.Click();
+
+            var questionHeader = driver.FindElement(By.ClassName("question-hyperlink"));
+            string questionHeaderText = questionHeader.Text;
+            driver.Quit();
+
+            Assert.IsTrue(questionHeaderText == "emplace_back для шаблонного конструктора");
+        }
+    }
+}
